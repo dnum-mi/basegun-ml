488f694380401bfaa12e1d48b612f098c708c205
aurelienmart <aurelien.martinez@wavestone.com>
2024-09-16 17:03:34 +0200

    refactor: :building_construction: Change the structure of the repo and add documentation

---
+++ Package/tests/test_classification.py
@@ -0,0 +1,147 @@
+from basegun_ml.classification import get_typology
+import os
+
+
+this_dir, this_filename = os.path.split(__file__)
+
+
+def to_bytes(img):
+    with open(img, "rb") as file:
+        image_bytes = file.read()
+    return image_bytes
+
+
+class TestTypology:
+    def test_verrou(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/a_verrou.jpg"))
+        assert pred[0] == "epaule_a_verrou"
+
+    def test_autre_pistolet(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/autrepistolet.jpg")
+        )
+        assert pred[0] == "autre_pistolet"
+
+    def test_epaule_ancien(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/epaule_ancien.jpg")
+        )
+        assert pred[0] == "epaule_mecanisme_ancien"
+
+    def test_levier_sous_garde(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/levier_sous_garde.jpg")
+        )
+        assert pred[0] == "epaule_a_levier_sous_garde"
+
+    def test_pistolet_ancien(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/pistolet_ancien.jpg")
+        )
+        assert pred[0] == "pistolet_mecanisme_ancien"
+
+    def test_pistolet_semi_auto(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/pistolet_semi_auto.jpg")
+        )
+        assert pred[0] == "pistolet_semi_auto_moderne"
+
+    def test_pompe(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/pompe.jpg"))
+        assert pred[0] == "epaule_a_pompe"
+
+    def test_revolver(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/revolver.jpg"))
+        assert pred[0] == "revolver"
+
+    def test_semi_auto_chasse(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_chasse.jpg")
+        )
+        assert pred[0] == "epaule_semi_auto_style_chasse"
+
+    def test_semi_auto_mil20(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_mil_20.jpg")
+        )
+        assert pred[0] == "epaule_semi_auto_style_militaire_milieu_20e"
+
+    def test_semi_auto_mil_autre(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_mil_autre.jpg")
+        )
+        assert pred[0] == "semi_auto_style_militaire_autre"
+
+    def test_un_coup(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/un_coup.jpg"))
+        assert pred[0] == "epaule_a_un_coup_par_canon"
+
+
+class TestConfidence:
+    def test_verrou(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/a_verrou.jpg"))
+        assert pred[2] == "high"
+
+    def test_autre_pistolet(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/autrepistolet.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_epaule_ancien(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/epaule_ancien.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_levier_sous_garde(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/levier_sous_garde.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_pistolet_ancien(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/pistolet_ancien.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_pistolet_semi_auto(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/pistolet_semi_auto.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_pompe(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/pompe.jpg"))
+        assert pred[2] == "high"
+
+    def test_revolver(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/revolver.jpg"))
+        assert pred[2] == "high"
+
+    def test_semi_auto_chasse(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_chasse.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_semi_auto_mil20(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_mil_20.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_semi_auto_mil_autre(self):
+        pred = get_typology(
+            to_bytes(this_dir + "/tests_images/test_typo/semi_auto_mil_autre.jpg")
+        )
+        assert pred[2] == "high"
+
+    def test_un_coup(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/un_coup.jpg"))
+        assert pred[2] == "high"
+
+    def test_autre(self):
+        pred = get_typology(to_bytes(this_dir + "/tests_images/test_typo/DTNUM.png"))
+        assert pred[2] == "low"
--- README.md
+++ README.md
@@ -1,9 +1,27 @@
+[![PyPI](https://img.shields.io/pypi/v/basegun-ml)](https://pypi.org/project/basegun-ml/)
+![PyPI - License](https://img.shields.io/pypi/l/basegun-ml)
+
 # Basegun-ml
 
-Code for creating the neural networks used in repo [Basegun](https://github.com/dnum-mi/Basegun) and in website [basegun](https://basegun.interieur.gouv.fr/) .  
-Check out [our wiki](https://github.com/dnum-mi/Basegun-ml/wiki) to learn more about the research for this code !
-* [Classification](https://github.com/dnum-mi/Basegun-ml/tree/master/classification): code for training image classifier neural networks.
-* [Measure module](https://github.com/dnum-mi/Basegun-ml/tree/master/measure_module): code for training the measure module algorithms to determine the weapon length and barrel length
-* [MLpackage](https://github.com/dnum-mi/Basegun-ml/tree/master/MLpackage): code of the basegun_ml package
+The Basegun-ml repository contains all the research code based on Machine Learning (ML) used in the [Basegun app](https://github.com/dnum-mi/Basegun), a tool designed to assist in the identification and legal categorization of firearms in France.
 
-It uses YOLO -> /!\ Licence
+The repository includes two main folders:
+
+- **Package Folder**: This contains the Python package used by the Basegun backend to run various machine learning algorithms. The package is available on pip:
+```
+pip install basegun-ml
+```
+
+- **Research Folder**: This contains all the experimental work that led to the creation of features in Basegun.
+
+Currently, Basegun has three main ML features:
+
+1. **Gun Mechanism Classification**: This feature categorizes an image into a list of families representing different firearm mechanisms. The classification is based on descriptive, objective criteria that are independent of legal classification.
+
+2. **Measure Length Module**: Measuring the overall length of a firearm or its barrel length is crucial for its legal classification. In France, the classification of long guns depends on these measurements. This module measures these lengths using an image.
+
+3. **Alarm Gun Recognition**: An alarm gun is a type of blank gun recognized as an alarm by French legislation. These guns are considered impossible to modify to make them lethal. The associated algorithm detects alarm guns using markings on the weapon.
+
+For more information, you can check our [Wiki](https://github.com/dnum-mi/basegun-ml/wiki)
+
+**Note**: Some models of Basegun-ml are based on YOLOv8 from [Ultralytics](https://github.com/ultralytics/ultralytics). To comply with the AGPL-3.0 license, Basegun-ml and all code using Basegun-ml must be under the same AGPL-3.0 license.
